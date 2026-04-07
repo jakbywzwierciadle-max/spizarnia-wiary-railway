c30293a070ae339daf442e459649803bcef4ab8a
jakbywzwierciadle-max <jakbywzwierciadle@gmail.com>
2026-04-07 15:55:17 +0200

    Update download.js

--- download.js
+++ download.js
@@ -6,7 +6,6 @@ import fs from "fs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const TARGET_DIR = __dirname;
 const CHANNEL_URL = "https://www.youtube.com/@spizarniawiary/videos";
 
 function execPromise(cmd) {
@@ -21,18 +20,18 @@ function execPromise(cmd) {
 export default async function downloadLatest() {
   console.log("🎧 Checking YouTube channel with cookies (Base64)...");
 
-  if (!process.env.YTDLP_COOKIES_BASE64) {
+  const base64 = process.env.YTDLP_COOKIES_BASE64;
+  if (!base64) {
     console.log("❌ Missing YTDLP_COOKIES_BASE64 env variable.");
     return;
   }
 
-  const decoded = Buffer.from(process.env.YTDLP_COOKIES_BASE64, "base64").toString("utf8");
-
+  const decoded = Buffer.from(base64, "base64").toString("utf8");
   const cookiesPath = path.join(__dirname, "cookies.txt");
-  fs.writeFileSync(cookiesPath, decoded);
 
-  const output = path.join(TARGET_DIR, "%(title)s.%(ext)s");
+  fs.writeFileSync(cookiesPath, decoded, { encoding: "utf8" });
 
+  const output = path.join(__dirname, "%(title)s.%(ext)s");
   const cmd = `yt-dlp --cookies "${cookiesPath}" --playlist-end 1 -x --audio-format mp3 -o "${output}" "${CHANNEL_URL}"`;
 
   console.log("⬇️ Downloading latest video audio...");
