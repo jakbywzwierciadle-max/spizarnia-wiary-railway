22e314307e8ef7d95822cd1b516f547979b28f57
jakbywzwierciadle-max <jakbywzwierciadle@gmail.com>
2026-04-07 11:21:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,15 @@ async function fetchLatestVideos() {
     );
   }
 
+  // --- FIX FOR RAILWAY DEPLOYMENT ---
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
+});
+
+
   generateFeed(videoIds);
   cleanupOldFiles();
 }
