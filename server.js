815dfb943249f88552550c08623dbfd9081c4ea8
jakbywzwierciadle-max <jakbywzwierciadle@gmail.com>
2026-04-08 15:11:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
-import downloadLatest from "./download.js";
+import { downloadLatest } from "./download.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
